86893430328ae61017a72fb4f96ce55931b4d958
iget-jmoreira <jmoreira@iget.com.br>
2015-06-11 20:05:17 -0300

    create base

---
+++ src/Models/User.java
@@ -0,0 +1,23 @@
+package Models;
+
+public class User {
+	public void createUser(String email, String username, String password, String password_verify, String accountType){
+		
+	}
+	
+	public void editUser(int user_id, String password){
+		
+	}
+	
+	public void deleteUser(int user_id, String password){
+		
+	}
+	
+	public void selectUser(int user_id){
+		
+	}
+	
+	public void listUsers(){
+		
+	}
+}
