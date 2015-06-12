af6d3ed04f65e5643ca8a0a160d11842c4759a02
iget-jmoreira <jmoreira@iget.com.br>
2015-06-11 23:00:13 -0300

    criando base

--- src/Views/DefaultSettings.java
+++ src/Views/DefaultSettings.java
@@ -11,15 +11,15 @@ import javax.swing.JPanel;
 
 public class DefaultSettings {
 	public DefaultSettings(JFrame frame, JPanel panel){
-		frame.setLayout(null);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		double x = (screenSize.getWidth() / 2) - ((screenSize.getWidth() - 100) / 2);
 		double width = screenSize.getWidth() - 100;
 		double height = screenSize.getHeight() - 180;
 		frame.setBounds((int)x, 80,(int) width,(int) height);
+		panel.setLayout(null);
 		panel.setBackground(Color.WHITE);
-		JLabel top = new JLabel("Financial System");
-		top.setBounds(0, 0, 150, 140);
+		JLabel top = new JLabel("<html><p style='font-size:28px;'>Financial System</p></html>");
+		top.setBounds((int)((screenSize.getWidth() / 2) - 202), 20, 344, 45);
 		panel.add(top);
 		
 	}
