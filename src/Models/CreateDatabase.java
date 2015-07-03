e53cfaf06a10792509e03de9016134697db96469
iget-jmoreira <jmoreira@iget.com.br>
2015-07-02 23:49:08 -0300

    backup

--- src/Models/CreateDatabase.java
+++ src/Models/CreateDatabase.java
@@ -43,7 +43,9 @@ public class CreateDatabase {
 			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users ("
 					+ "id INT NOT NULL AUTO_INCREMENT,"
 					+ "company_id INT,"
-					+ "name VARCHAR(255) NOT NULL,"
+					+ "username VARCHAR(255) NOT NULL,"
+					+ "password VARCHAR(255) NOT NULL,"
+					+ "dob DATE,"
 					+ "contact_phone VARCHAR(255),"
 					+ "contact_email VARCHAR(255) NOT NULL,"
 					+ "FOREIGN KEY (company_id) REFERENCES companies(id),"
@@ -52,17 +54,49 @@ public class CreateDatabase {
 			stmt.close();
 			
 			// Cria Tabela Products
+			stmt = conn.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS products("
+					+ "id INT NOT NULL AUTO_INCREMENT,"
+					+ "code VARCHAR(255) NOT NULL,"
+					+ "name VARCHAR(255) NOT NULL,"
+					+ "cost DECIMAL(12,2) NOT NULL,"
+					+ "PRIMARY KEY (id)"
+					+ ") ENGINE = InnoDB");
+			stmt.close();
 			
 			// Cria Tabela Payments
+			stmt = conn.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS payments("
+					+ "id INT NOT NULL AUTO_INCREMENT,"
+					+ "description VARCHAR(255) NOT NULL,"
+					+ "quantity int(50) NOT NULL,"
+					+ "product_id INT,"
+					+ "account_id INT,"
+					+ "date DATE NOT NULL,"
+					+ "value DECIMAL(12,2) NOT NULL,"
+					+ "FOREIGN KEY (product_id) REFERENCES products(id),"
+					+ "FOREIGN KEY (account_id) REFERENCES accounts(id),"
+					+ "PRIMARY KEY (id)"
+					+ ") ENGINE = InnoDB");
+			stmt.close();
 			
 			// Cria Tabela Receipts
+			stmt = conn.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS payments("
+					+ "id INT NOT NULL AUTO_INCREMENT,"
+					+ "description VARCHAR(255) NOT NULL,"
+					+ "quantity int(50) NOT NULL,"
+					+ "product_id INT,"
+					+ "account_id INT,"
+					+ "date DATE NOT NULL,"
+					+ "value DECIMAL(12,2) NOT NULL,"
+					+ "FOREIGN KEY (product_id) REFERENCES products(id),"
+					+ "FOREIGN KEY (account_id) REFERENCES accounts(id),"
+					+ "PRIMARY KEY (id)"
+					+ ") ENGINE = InnoDB");
+			stmt.close();
 			
-			// Cria Tabela CashFlow
-			
-			
-			System.out.println("ok!");
-			
-			
+			System.out.println("Banco e Tabelas criados com sucesso!");
 			
 		} catch(ClassNotFoundException e){
 			System.out.println("Erro no Driver "+e.getMessage());
