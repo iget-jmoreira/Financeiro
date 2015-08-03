b452a6fd03bc365d93e4c777b753454286cbd48c
Airton <jmoreira@iget.com.br>
2015-08-03 18:09:49 -0300

    Update BaseController.java

--- src/Controllers/BaseController.java
+++ src/Controllers/BaseController.java
@@ -1,6 +1,7 @@
 package Controllers;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class BaseController {
 	public void goTo(JFrame actual, JFrame destination){
@@ -8,5 +9,8 @@ public class BaseController {
 		actual.dispose();
 		destination.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		destination.setVisible(true);
+	}
+	public void popUp(String title, String message){
+		JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
 	}
 }
