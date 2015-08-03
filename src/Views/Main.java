21684625981277f6054f574be8e71e2009ccd567
Airton <jmoreira@iget.com.br>
2015-08-03 18:12:39 -0300

    Update Main.java

--- src/Views/Main.java
+++ src/Views/Main.java
@@ -6,13 +6,18 @@ public class Main {
 	public static void main(String[] args){
 		//instacia elementos para serem visualizados		
 		SelectSystem page = new SelectSystem();
+//		Login page = new Login("personal");
+//		SignUp page = new SignUp("personal");
+//		RecoveryCheckEmail page = new RecoveryCheckEmail("personal");
+//		RecoveryChangePass page = new RecoveryChangePass("email@email.com", "Who was my fisrt teacher?", "personal");
+		
 		page.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		page.setVisible(true);
 		
 //		Deixe o codigo acima comentado e a linha de baixo descomentada
 //		para criar os bancos de dados do sistema.
 		
-//		new CreateDatabase();
+//		new Models.CreateDatabase();
 		
 	}
 }
