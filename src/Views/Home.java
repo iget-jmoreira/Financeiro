10a399fe9b13e795333038b2a134f5b75a2cf6a1
Airton <jmoreira@iget.com.br>
2015-08-03 18:12:06 -0300

    Create Home.java

---
+++ src/Views/Home.java
@@ -0,0 +1,14 @@
+package Views;
+
+import javax.swing.JFrame;
+
+public class Home extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Home(String systemType){
+		
+	}
+}
